9838e19bbbf95578102640557b69f97862f3be32
anbak98 <anbak98@gmail.com>
2025-02-19 12:52:19 +0900

    [ feature ] Sync a player hand. everything works except rotation

--- MetaverseProject/Assets/Scripts/Network/GameManager.cs
+++ MetaverseProject/Assets/Scripts/Network/GameManager.cs
@@ -3,17 +3,71 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
-public class GameManager : NetworkBehaviour
+public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private NetworkManager m_NetworkMnaager;
+    NetworkVariable<int> yesVote = new NetworkVariable<int>(0);
+
+    private void Awake()
     {
-        
+        m_NetworkMnaager = GetComponent<NetworkManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+        if (!m_NetworkMnaager.IsClient && !m_NetworkMnaager.IsServer)
+        {
+            StartButton();
+        }
+        else
+        {
+            StatusLabels();
+
+            SubmitEnterGame();
+        }
+
+        GUILayout.EndArea();
+    }
+
+    void StartButton()
+    {
+        if (GUILayout.Button("Host")) m_NetworkMnaager.StartHost();
+        if (GUILayout.Button("Client")) m_NetworkMnaager.StartClient();
+        if (GUILayout.Button("Server")) m_NetworkMnaager.StartServer();
+    }
+
+    void StatusLabels()
+    {
+        var mode = m_NetworkMnaager.IsHost ?
+            "Host" : m_NetworkMnaager.IsServer ? "Server" : "Client";
+
+        GUILayout.Label("Transport: " +
+            m_NetworkMnaager.NetworkConfig.NetworkTransport.GetType().Name);
+        GUILayout.Label("Node: " + mode);
+        if(m_NetworkMnaager.IsServer) GUILayout.Label("EnterCount: " + yesVote.Value + "/" + m_NetworkMnaager.ConnectedClients.Count);
+    }
+
+    void SubmitEnterGame()
+    {
+        if (GUILayout.Button(m_NetworkMnaager.IsServer ? "Vote" : "Request Vote"))
+        {
+            if(m_NetworkMnaager.IsClient) IncreaseVote();
+            if (yesVote.Value == m_NetworkMnaager.ConnectedClients.Count)
+            {
+                if (m_NetworkMnaager.IsServer) m_NetworkMnaager.SceneManager.LoadScene("GameScene", UnityEngine.SceneManagement.LoadSceneMode.Single);
+            }       
+        }
+    }
+
+    [Rpc(SendTo.Server)]
+    void IncreaseVote()
+    {
+        yesVote.Value++;
+        Debug.Log(yesVote.Value);
     }
 }
